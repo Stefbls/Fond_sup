25b98466a583de95902fe4ee074024b0b40842b9
Stefbls <stefano.bilosi@gmail.com>
2025-11-04 22:13:49 +0100

    Fix LaTeX formatting for E_x and E_y
    Updated LaTeX formatting for E_x and E_y outputs.

--- app.py
+++ app.py
@@ -16,15 +16,15 @@ if V > 0:
     Ex = My / V
     Ey = Mx / V
     st.subheader("Risultati")
-    st.subheader("Risultati")
-
+    
     # Ex
     st.latex(r"E_x = \frac{M_y}{V}")
-    st.latex(fr"E_x = \frac{{{My:.2f}\ \text{kNm}}}{{{V:.2f}\ \text{kN}}} = {Ex:.4f}\ \text{ m}")
+    st.latex(fr"E_x = \frac{{{My:.2f}\ \text{{kNm}}}}{{{V:.2f}\ \text{{kN}}}} = {Ex:.4f}\ \text{{m}}")
 
     # Ey
     st.latex(r"E_y = \frac{M_x}{V}")
-    st.latex(fr"E_y = \frac{{{Mx:.2f}\ \text{kNm}}}{{{V:.2f}\ \text{kN}}} = {Ey:.4f}\ \text{ m}")
+    st.latex(fr"E_y = \frac{{{Mx:.2f}\ \text{{kNm}}}}{{{V:.2f}\ \text{{kN}}}} = {Ey:.4f}\ \text{{m}}")
+
 
 
     # Controllo se l’eccentricità è entro il nucleo centrale
